4bced0ff62e3f59e8986961c385eae3f79b0be6e
SALEH-abdulwahab <saleh200228@gmail.com>
2025-07-28 14:49:54 +0300

    cm

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import blogData from "../data/blog";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Header from "./Header";
+console.log(blogData);
+
+function App() {
+  const { name, image, about, posts } = blogData
+  return (
+    <div className="App">
+      
+      <Header name={name} />
+      <About image={image} about={about} />
+      <ArticleList posts={posts} />
+    </div>
+  );
+}
+
+export default App;
